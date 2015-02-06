2d60f66dfb9146e7376743b8de49a9fef5b038ce
Kristoffer Brabrand <kristoffer@brabrand.no>
2015-02-06 13:11:33 +0100

    Initial commit

---
+++ nrk-parser.php
@@ -0,0 +1,60 @@
+<?php
+
+$opts = array(
+  'http'=>array(
+    'method'=>"GET",
+    'header'=>"Cookie: NRK_PLAYER_SETTINGS_TV=devicetype=desktop&preferred-player-odm=hlslink&preferred-player-live=hlslink&max-data-rate=3500;"
+  )
+);
+
+$context = stream_context_create($opts);
+
+$html = file_get_contents($argv[1], false, $context);
+
+preg_match('/data-video-id\s?=\s?"(.*)"/', $html, $programIdMatch);
+preg_match('/data-subtitlesurl\s?=\s?"(.*)"/', $html, $subtitleUrlMatch);
+preg_match('/href="(.*\.m3u8)"/', $html, $hlsMediaMatch);
+preg_match('/springStreamStream":"([\-\.a-zA-ZæåøÆÅØ]+\/)*([^"]+)/', $html, $keyMatch);
+
+if (!$programIdMatch) {
+    echo 'Program id not found';
+}
+
+if (!$keyMatch) {
+    echo 'Program key not found';
+    exit;
+}
+
+if (!$hlsMediaMatch) {
+    echo 'No HLS link found on page';
+    exit;
+}
+
+$programId   = $programIdMatch[1];
+$key         = str_replace('.' . $programId, '', $keyMatch[2]);
+$subtitleUrl = null;
+
+if ($subtitleUrlMatch) {
+    $subtitleUrl = sprintf('http://tv.nrk.no%s', $subtitleUrlMatch[1]);
+}
+
+$m3u8 = file_get_contents($hlsMediaMatch[1]);
+$m3u8Lines = explode("\n", $m3u8);
+
+$maxBandwidth = 0;
+$streamLink   = '';
+
+foreach ($m3u8Lines as $index => $line) {
+    if (substr($line, 0, 1) !== '#') {
+        continue;
+    }
+
+    preg_match('/BANDWIDTH=([0-9]+),/', $line, $bandwidthMatch);
+
+    if ($bandwidthMatch && $bandwidthMatch[1] > $maxBandwidth) {
+        $maxBandwidth = $bandwidthMatch[1];
+        $streamLink    = $m3u8Lines[$index + 1];
+    }
+}
+
+echo sprintf('%s %s %s', $key, $streamLink, $subtitleUrl);
---
+++ nrk-subtitle-downloader.php
@@ -0,0 +1,68 @@
+<?php
+
+// Too few arguments
+if (count($argv) < 3) {
+    exit;
+}
+
+$key         = $argv[1];
+$subtitleUrl = $argv[2];
+$dstFile     = getcwd() . DIRECTORY_SEPARATOR . $key . '.nb.srt';
+
+// Skip if subtitle file exists already
+if (is_file($dstFile)) {
+    echo 1;
+    exit;
+}
+
+$output = fopen($dstFile, 'w');
+
+$subtitleXML = file_get_contents($subtitleUrl);
+
+$lines = explode("\n", $subtitleXML);
+
+// Subtitle sequence number
+$index = 0;
+
+foreach ($lines as $line) {
+    if (preg_match('/<p begin="(\d{2}:\d{2}:\d{2}).(\d{3})" dur="(\d{2}):(\d{2}):(\d{2}).(\d{3})"( style="([a-zA-Z]+)")?>(.*?)<\/p>/', $line, $match)) {
+        list(, $begin, $beginDecimals, $dh, $dm, $ds, $dd, , $style, $text) = $match;
+
+        $prefix = '';
+        $suffix = '';
+
+        if ($style === 'italic') {
+            $prefix = '<p>';
+            $suffix = '</p>';
+        }
+
+        $startTime = $begin . '.' . $beginDecimals;
+        $endTime   = new DateTime($begin);
+
+        // Add duration
+        if ($dh > 0 || $dm > 0 || $ds > 0) {
+            $endTime->add(new DateInterval(sprintf('PT%sH%sM%sS', (int) $dh, (int) $dm, (int) $ds)));
+        }
+
+        // Add decimal overflow
+        if ($beginDecimals + $dd > 1000) {
+            $endTime->add(new DateInterval('PT1S'));
+        }
+
+        // Format time and append decimals
+        $endTime = $endTime->format('H:i:s') . '.' . str_pad(($beginDecimals + $dd) % 1000, 3, '0', STR_PAD_LEFT);
+
+        $line = implode(PHP_EOL, [
+            ++$index,
+            sprintf('%s --> %s', $startTime, $endTime),
+            str_replace('<br />', PHP_EOL, $text),
+            PHP_EOL
+        ]);
+
+        fwrite($output, $line);
+    }
+}
+
+fclose($output);
+
+return 1;
